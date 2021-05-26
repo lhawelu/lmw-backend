7409fc0882f84c0def9284b5ce72e85615827eb9
Larry Hawelu <lhawelu@gmail.com>
2021-05-26 16:59:59 -0700

    adding order creation route and controller

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -4,5 +4,21 @@ class Api::V1::OrdersController < ApplicationController
     orders = @@user.orders.all.map{|order| OrderSerializer.new(order)}
     render json: { orders: orders }, status: :ok
   end
+
+  def create
+    order = Order.new(order_params)
+    order.user = @@user
+    order.save
+    if order.valid?
+      params[:items].each{ |item| order.items << Item.find(item) }
+      order.save
+      render json: { message: 'order created'}, status: :created
+    else
+      render json: { error: 'failed to create order' }, status: :not_acceptable
+    end
+  end
   
+  def order_params
+    params.require(:order).permit(:total_amount, :tax_amount, :subtotal)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :orders, only: [:index]
+      resources :orders, only: [:index, :create]
       resources :users, only: [:create]
       resources :items, only: [:index]
       post '/login', to: 'auth#create'
