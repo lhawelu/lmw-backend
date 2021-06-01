cea32997a9fd915d4743fde78b1a54aa15783714
Larry Hawelu <lhawelu@gmail.com>
2021-05-31 20:19:41 -0700

    create user added with some validations

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,11 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    @user = User.create(user_params)
-    if @user.valid?
-      @token = encode_token(user_id: @user.id)
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    user = User.create(user_params)
+    if user.valid?
+      render json: { message: 'Account created. Pleaes log in.' }, status: :created
     else
-      render json: { error: 'failed to create user' }, status: :not_acceptable
+      render json: { message: user.errors.full_messages }, status: :not_acceptable
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, uniqueness: true
+  validates :email_address, uniqueness: true
 
   has_many :orders, dependent: :destroy
   
